1bee9be8ca9de12498d08c39fd64b51031c4b61c
gonzacze <gonzalocze1@gmail.com>
2022-10-01 17:57:06 -0300

    Realizando vistas y rutas

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
-const express = require("express");
-const path = require("path");
-const app = express();
+// const express = require("express");
+// const path = require("path");
+// const app = express();
 // const port = process.env.PORT || 8080;
 
 
 
 // port & server
-app.listen(5000, (req, res) => {
-  console.log("server corriendo en el puerto 5000");
-});
+// app.listen(5000, (req, res) => {
+//   console.log("server corriendo en el puerto 5000");
+// });
 
-app.set("views", path.join(__dirname, "./src/views"));
+// app.set("views", path.join(__dirname, "./src/views"));
+
+
+const express = require("express");
+const app = express ();
+const path =  require ("path");
+// const mainRoute = require('./routes/mainRoute');
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'))
+
+const PORT = 3000;
+app.listen(PORT, ()=>{
+    console.log('Server corriendo en port: ', PORT)
+})
+
+app.use(express.static(path.join(__dirname, '../public')))
+
+app.get('/', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, '../views/index.html'))
+})
