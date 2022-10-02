5be89dfa94cd7215a3b66a327a2152571a65dbd3
AleBianchi71 <bianchialejandro998@gmail.com>
2022-10-02 00:32:42 -0300

    se agrego nuevas vistas html incluido sequelizerc

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
-// const express = require("express");
-// const path = require("path");
-// const app = express();
-// const port = process.env.PORT || 8080;
-
-
-
-// port & server
-// app.listen(5000, (req, res) => {
-//   console.log("server corriendo en el puerto 5000");
-// });
-
-// app.set("views", path.join(__dirname, "./src/views"));
-
 
 const express = require("express");
-const app = express ();
+const app = express();
 const path =  require ("path");
 // const mainRoute = require('./routes/mainRoute');
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
+
 
 const PORT = 3000;
 app.listen(PORT, ()=>{
@@ -30,4 +17,10 @@ app.use(express.static(path.join(__dirname, '../public')))
 
 app.get('/', (req, res)=>{
   res.sendFile(path.resolve(__dirname, '../views/index.html'))
+})
+app.get('/', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, '../views/listado.html'))
+})
+app.get('/', (req, res)=>{
+  res.sendFile(path.resolve(__dirname, '../views/login.html'))
 })
