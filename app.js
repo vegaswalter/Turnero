d7987fbfab6d49f9fb392877b20ccc1603d82d67
AleBianchi71 <bianchialejandro998@gmail.com>
2022-10-04 10:51:29 -0300

    creando error 404

--- app.js
+++ app.js
@@ -19,7 +19,8 @@ app.set('views', path.join(__dirname, './src/views'))
 // static
 app.use(express.static(path.join(__dirname, 'public')))
 
-//routes
+//routes  
+//renderiza todas las vistas para no estar una a una
 app.use("/", require("./src/routes/index.routes"));
 
 //a medida que se avance ir desbloquedo las rutas sino
@@ -33,6 +34,12 @@ app.use("/", require("./src/routes/index.routes"));
 // app.get('/listado', (req, res)=>{
 //   res.sendFile(path.resolve(__dirname, './src/views/listado.html'))
 // })
+
+app.use((req,res,next)=>{
+  res.status(404).render("not-found");
+})
+
+//app.ErrorDocument 404/errores/error404.html
 
 app.get('/turnos', (req, res)=>{
   res.sendFile(path.resolve(__dirname, './src/views/turnos.html'))
