8cf2eb2127eb9dc76eac31b6f3fd7bead9f00de6
gonzacze <gonzalocze1@gmail.com>
2022-10-10 11:10:47 -0300

    Cambios

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const cors = require("cors");
 const methodOverride = require("method-override");
 const path = require("path");
 const session = require('express-session');
+const bodyParser = require ("body-parser")
+
+const { sequelize } = require("./src/database/models");
 const PORT = process.env.PORT || 3000;
 
 
@@ -22,6 +25,10 @@ app.use(cors());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, './src/views'))
 
+//Para poder rellenar el req.body
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
+
 // static
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(session({secret:'solo el admin puede ver esto'}));
@@ -36,6 +43,11 @@ app.use("/", require("./src/routes/index.routes"));
 app.use((req,res,next)=>{
   res.status(404).render("not-found");
 })
+
+// sequelize.sync({force:false}).then(() => {
+//   console.log("Se Levanto la base de datos")
+// }).catch((error) => {console.log("Se encontro un error", error)} )
+
 
 
 
