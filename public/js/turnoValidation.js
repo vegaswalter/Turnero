c8e9512ca8275cbd7dc7cd852fe8aad004f128fe
vegaswalter <damianvegasrodriguez@gmail.com>
2022-10-13 16:09:32 -0300

    Implementacion de estados

--- public/js/turnoValidation.js
+++ public/js/turnoValidation.js
@@ -4,7 +4,6 @@ window.addEventListener("load", () => {
   let ulErrores = document.querySelector("#errores");
 
   inputCode.focus();
-
   let arrayInput = [inputCode];
 
   formulario.addEventListener("submit", function (e) {
@@ -12,30 +11,40 @@ window.addEventListener("load", () => {
     ulErrores.innerHTML = "";
     arrayInput.forEach(function (input) {
       input.nextElementSibling.innerHTML = "";
+      if (input.value.length == 0 || input.value == " ") {
+        input.classList.add("is-invalid");
+        errores++;
+        ulErrores.innerHTML += `<li>El campo ${input.dataset.nombre} no puede estar vacío</li>`;
+        input.nextElementSibling.innerHTML = "El campo no puede estar vacío";
+        return;
+      }
+
+      if (input.value != "") {
+        input.classList.remove("is-invalid");
+        input.nextElementSibling.innerHTML = "";
+      }
 
       if (input.dataset.nombre == "code" && input.value.length > 3) {
         input.classList.add("is-invalid");
         errores++;
-        ulErrores.innerHTML += (
-          <li>El codigo no puede tener mas de 3 caracteres</li>
-        );
-        input.nextElementSibling.innerHTML =
-          "El largo del nombre no puede ser tan largo";
+        ulErrores.innerHTML += `<li>El codigo no puede ser mas de 3 caracteres</li>`;
+       
       }
 
-      if (input.dataset.nombre == "code" && input.value.length < 2) {
+      if (input.dataset.nombre == "code" && input.value.length < 3) {
         input.classList.add("is-invalid");
         errores++;
-        ulErrores.innerHTML += (
-          <li>El codigo no puede tener menos de 3 caracteres</li>
-        );
-        input.nextElementSibling.innerHTML =
-          "El largo del codigo no puede ser tan corto";
+        ulErrores.innerHTML += `<li>El codigo no puede ser menos de 3 caracteres</li>`;
       }
 
       if (errores > 0) {
         e.preventDefault();
+        ulErrores.classList.add("alert-warning");
       }
     });
+
+ 
+
+  
   });
-});
+})
