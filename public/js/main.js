f0e039c834d969df68efa1e516c12864427dcaa1
damianvegasrodriguez@gmail.com <damianvegasrodriguez@gmail.com>
2023-05-03 21:12:12 -0300

    Temperature and Date

--- public/js/main.js
+++ public/js/main.js
@@ -8,3 +8,19 @@ abrir.addEventListener("click", () => {
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
 })
+
+
+const claveAPI = "c3f3a4ab820e63555dcda3351f1f214c";
+const ciudad = "Buenos Aires,AR";
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad}&units=metric&appid=${claveAPI}`;
+
+      fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          const temperaturaActual = data.main.temp;
+          const elementoTemperatura = document.getElementById("temperatura");
+          elementoTemperatura.textContent = `${temperaturaActual}°C`;
+        })
+    .catch((error) => console.error(error));
+        
+  
